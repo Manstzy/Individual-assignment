90ae9df7a973b7d9b28024fafe1a8cb17fd1d08d
Manstzy <muh.arhief20@gmail.com>
2024-01-20 13:29:17 +0700

    tambah fitur responsive dan penyempurnaan lainnya

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}" , "./index.html"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
+    screens: {
+      phone: "390px",
+      sm: "490px",
+      md: "640px",
+      lg: "768px",
+      xl: "1024",
+      xxl: "1280px",
+    },
     extend: {
       fontFamily: {
-        Poppins: ['Wix Madefor Text', 'sans-serif'],
-      },
-      screens: {
-        'phone': '466px',
+        Poppins: ["Wix Madefor Text", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
