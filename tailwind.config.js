b096f248abe5eb536af9f5df55b54011426aa37a
Manstzy <muh.arhief20@gmail.com>
2024-01-20 16:04:47 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     screens: {
+      miniPhone:"320px",
       phone: "390px",
       sm: "490px",
+      sMd: "520px",
       md: "640px",
       lg: "768px",
       xl: "1024",
